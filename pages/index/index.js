528b31a19c9a25cbf24db13e37aa6f45e63c8fd7
qiurongfeng <qiurongfeng@pku.edu.cn>
2018-05-04 14:13:59 +0800

    v1.1 2018.5.4

---
+++ pages/index/index.js
@@ -0,0 +1,29 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  // data: {
+  //   motto: 'Hello World',
+  //   userInfo: {},
+  //   hasUserInfo: false,
+  //   canIUse: wx.canIUse('button.open-type.getUserInfo')
+  // },
+  //事件处理函数
+
+  data: {
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    indicatorDots: false,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000
+  },
+  onLoad: function () {
+
+  },
+
+})
